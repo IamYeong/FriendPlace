a9c91883fd13f2dfb89c5adda226a31b6a63a006
IamYeong <wjdrhkddud2@gmail.com>
2021-03-16 19:34:20 +0900

    navigation xml file creating

--- app/src/main/java/com/iamyeong/myfriendplace/MainActivity.java
+++ app/src/main/java/com/iamyeong/myfriendplace/MainActivity.java
@@ -1,17 +1,47 @@
 package com.iamyeong.myfriendplace;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.material.appbar.CollapsingToolbarLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        /*
+        CollapsingToolbarLayout collapsingToolbarLayout = findViewById(R.id.collapsing_toolbar);
+        NavController controller = Navigation.findNavController(this, R.id.nav_fragment);
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(controller.getGraph()).build();
+        Toolbar toolbar = findViewById(R.id.toolbar_main);
+        NavigationUI.setupWithNavController(collapsingToolbarLayout, toolbar, controller, appBarConfiguration);
+
+
+
+         */
+
 
 
     }
+
+
 }
